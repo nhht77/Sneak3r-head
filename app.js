f137a15c48dc3f4b0fd6f738d82d1439098ac382
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-01-12 08:54:43 +0200

    Connect to Mongo Atlas

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const express = require('express');
-const app     = express();
+const express  = require('express');
+const mongoose = require('mongoose');
+const app      = express();
+
+const db = require('./config/key').mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true }).then(() => console.log('DB Connected'))
+                    .catch(err => console.log(err));
 
 const PORT = 3000;
 
