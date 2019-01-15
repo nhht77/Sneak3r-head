4ee045d519d431ef20a40b26a619c05b41e4920f
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-01-15 15:45:11 +0200

    Fix login

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,7 @@ const jwt     = require('jsonwebtoken');
 const User = require('../../models/User');
 const configKey  = require('../../config/key').key
 
+
 router.get('/test', (req, res) => res.json({msg: 'user works'}));
 
 router.post('/register', (req, res) => {
@@ -32,8 +33,37 @@ router.post('/register', (req, res) => {
             })
         }
     });
+});
 
-})
+router.post('/login', (req, res) => {	
+    const errors = {};	
+    const {email, password} = req.body;	
+
+
+     User.findOne({email}).then( user => {	
+        if(!user){	
+            errors.email = "User is not found";	
+            res.status(404).send(errors)	
+        } 	
+
+         bcrypt.compare(password, user.password, (isMatch) => {	
+            if(!isMatch){	
+                const {_id: id, name, email, password } = user;	
+                const payload = {id, name, email, password};	
+
+                 jwt.sign(payload, configKey, {expiresIn:3600}, (err, token) => {	
+                    res.json({	
+                        success:true,	
+                        token: `bearer ${token}`	
+                    })	
+                })	
+            } else {	
+                errors.password = "The password is incorrect";	
+                res.status(404).send(errors);	
+            }	
+        })	
+    }).catch( err => console.log(err));	
+});
 
 
 
