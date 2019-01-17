383354754be500605c8e3f62dbb88fb4f4f35f20
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-01-17 21:16:50 +0200

    Login validate

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,7 +6,9 @@ const jwt     = require('jsonwebtoken');
 const User = require('../../models/User');
 const configKey  = require('../../config/key').key
 
-const isEmpty = value => value === null || value === undefined
+// const isEmpty = value => value === null || value === undefined
+const validateLoginInput = require("../../validation/login");
+// const isEmpty = require("../../validation/is-empty");
 
 router.get('/test', (req, res) => res.json({msg: 'user works'}));
 
@@ -36,53 +38,41 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {	
-    const errors = {};	
-    const {email, password} = req.body;	
-
-    if(isEmpty(email) && isEmpty(password)) {
-        errors.email = "Email must not empty";
-        errors.password = "Password must not empty";
-        res.status(404).json(errors);
-    }
-
-    if(isEmpty(email)) {
-        errors.email = "Email must not empty";
-        res.status(404).json(errors);
-    }
-
-    if(isEmpty(Password)) {
-        errors.password = "Password must not empty";
-        res.status(404).json(password);
-    } else {
-        return res.json({email, password});
-    }
-
+router.post('/login', (req, res) => {
+    // let errors = {}
+    const { isValid, errors } = validateLoginInput(req.body);
     
-
-
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+    
+    // if(isEmpty(req.body.email)){
+    //     errors.email = "Email must not be blank";
+    //     return res.status(404).json(errors);
+    // }
+    
     //  User.findOne({email}).then( user => {	
-    //     if(!user){	
-    //         errors.email = "User is not found";	
-    //         res.status(404).send(errors)	
-    //     } 	
-
-    //      bcrypt.compare(password, user.password, (isMatch) => {	
-    //         if(!isMatch){	
-    //             const {_id: id, name, email, password } = user;	
-    //             const payload = {id, name, email, password};	
-
-    //              jwt.sign(payload, configKey, {expiresIn:3600}, (err, token) => {	
-    //                 res.json({	
-    //                     success:true,	
-    //                     token: `bearer ${token}`	
-    //                 })	
-    //             })	
-    //         } else {	
-    //             errors.password = "The password is incorrect";	
-    //             res.status(404).send(errors);	
-    //         }	
-    //     })	
+        // if(!user){	
+            // errors.email = "User is not found";	
+            // res.status(404).send(errors)	
+        // } 	
+// 
+        //  bcrypt.compare(password, user.password, (isMatch) => {	
+            // if(!isMatch){	
+                // const {_id: id, name, email, password } = user;	
+                // const payload = {id, name, email, password};	
+// 
+                //  jwt.sign(payload, configKey, {expiresIn:3600}, (err, token) => {	
+                    // res.json({	
+                        // success:true,	
+                        // token: `bearer ${token}`	
+                    // })	
+                // })	
+            // } else {	
+                // errors.password = "The password is incorrect";	
+                // res.status(404).send(errors);	
+            // }	
+        // })	
     // }).catch( err => console.log(err));	
 });
 
--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,5 +1,7 @@
-export const isEmpty = value => 
+const isEmpty = value => 
              value === null ||
              value === undefined ||
              (typeof value === "string" && value.trim().length === 0) ||
              (typeof value === "object" && Object.keys(value).length === 0);
+
+module.exports = isEmpty;
