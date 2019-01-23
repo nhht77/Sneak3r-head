931ecad10eeb794ccd686bb2f6f236b03bb4bbea
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-01-23 11:58:07 +0200

    Adding Conditions

--- routes/api/conditions.js
+++ routes/api/conditions.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router  = express.Router();
 
+const Conditions = require('../../models/Condition');
+const validateConditionsInput = require('../../validation/conditions');
+
 router.get('/test', (req, res) => res.send({ msg: 'Conditions Test Routes Works'}));
+
+router.get('/', (req, res) => {
+    Conditions.find({}).then( (err, conditions) => {
+        if(err) res.json(err);
+        res.json(conditions);
+    })
+})
+
+router.post('/', (req, res) => {
+    const { isValid, errors } = validateConditionsInput(req.body);
+
+    if(!isValid){
+        res.status(400).json(errors);
+    }
+
+    res.json(req.body);
+})
 
 module.exports = router;
