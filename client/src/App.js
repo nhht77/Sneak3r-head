32dfee3ed2c14d20506fa86a2af3d7f962327dec
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-02-01 15:17:22 +0200

    Init React-Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
+import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
+import Landing from './components/Landing/Landing';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Header/>
-        
-      <Footer/>
-      </div>
+      <Router>
+        <div className="App">
+          <Header/>
+            <div className="page-container">
+              <Route exact path='/' component={Landing}/>
+            </div>
+          <Footer/>
+        </div>
 
+        </Router>
     );
   }
 }
+// {<Route exact path ="/" component={Landing}/>}
+
 
 export default App;
--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -7,36 +7,36 @@ export class Footer extends Component {
             <footer className="bg-b-dark">
                 <div className="container">
                     <div className="logo">
-                        Waves
+                        Sneak3r  Head
                     </div>
                     <div className="wrapper">
                         <div className="left">
                             <h2>Contact information</h2>
-                            <div className="business-nfo">
+                            <div className="business-info">
                                 <div className="tag">
                                     Tag Compass
-                                    <div className="nfo">
+                                    <div className="info">
                                         <div>Address</div>
                                         <div>Address Info</div>
                                     </div>
                                 </div>
                                 <div className="tag">
                                     Tag Phone
-                                    <div className="nfo">
+                                    <div className="info">
                                         <div>Phone</div>
                                         <div>Phone number</div>
                                     </div>
                                 </div>
                                 <div className="tag">
                                     Tag Clock
-                                    <div className="nfo">
+                                    <div className="info">
                                         <div>Working hours</div>
                                         <div>Working Hours Info</div>
                                     </div>
                                 </div>
                                 <div className="tag">
                                     Tag Envelope Email
-                                    <div className="nfo">
+                                    <div className="info">
                                         <div>Email</div>
                                         <div>Email Info</div>
                                     </div>
