3b9c24b576960d2b2e71714d9eb5d7bd3410dfe9
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-02-05 23:12:14 +0200

    Conditional Header nav

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,9 +1,32 @@
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 class Header extends Component {
 
-
     render() {
+        const {isAuthenticated} = this.props.auth;
+
+        const authLinks = (
+            <div className="top">
+                <Link to="/">My Cart</Link>
+                <Link to="/">My Account</Link>
+                <div className="logout-link"
+                    onClick={()=> {}}>
+                    Log out
+                </div>
+            </div>
+        );
+      
+        const guestLinks = (
+            <div className="top">
+                <Link to="/login">Log in</Link>
+                <Link to="/register">Register</Link>
+            </div>
+        );
+      
+
         return (
             <header className="bg-b-light">
                 <div className="container">
@@ -13,11 +36,10 @@ class Header extends Component {
                         </div>
                     </div>
                     <div className="right">
-                        <div className="top">
-                            Top
-                        </div>
+                            {isAuthenticated ? authLinks : guestLinks }
                         <div className="bottom">
-                            Bottom
+                            <Link to="/">Home</Link>
+                            <Link to="/shop">Shoes</Link>
                         </div>
                     </div>
                 </div>
@@ -26,5 +48,8 @@ class Header extends Component {
     }
 }
 
+const mapStateToProps = state => ({
+    auth: state.auth
+  });
 
-export default Header;
+export default connect(mapStateToProps, {})(Header);
