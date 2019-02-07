c6af0af626fbdbc8337bdb348cd58f3ed0011e4e
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-02-07 21:25:18 +0200

    Card Component

--- client/src/components/Common/Card/Card.js
+++ client/src/components/Common/Card/Card.js
@@ -1,23 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import DefaultButton from "../Button/DefaultButton";
 
 export class Card extends Component {
+
+    renderImages(img){
+        if(img.length > 0){
+            return img[0].url
+        } else {
+            return '/resources/images/not-available.png'
+        }
+    }
+    
   render() {
     return (
         <div className={`card-item-wrapper ${this.props.grid}`}>
-        <div
-            className="image"
-            // style={{ background:`url(${this.renderCardImage(this.props.images)}) no-repeat`}}
-        >
+        <div className="image"
+        style={{ 
+            background:`url(${this.renderImages(this.props.product.images)}) 
+            no-repeat`
+        }}>
         </div>
-            <div className="action-container">
-                <div className="tags">
-                    <div className="brand">{this.props.product.brand.name}</div>
-                    <div className="name">{this.props.product.name}</div>
-                    <div className="name">{this.props.product.price}</div>
+        <div className="action-container">
+            <div className="tags">
+                <div className="brand">{this.props.product.brand.name}</div>
+                <div className="name">{this.props.product.name}</div>
+                <div className="name">€ {this.props.product.price}</div>
+            </div>
+
+            { 
+                this.props.grid 
+                ?
+                <div className="description">
+                    <p>{this.props.product.description}</p>    
                 </div>
+                : null
+            }
+            <div className="actions">
+            <div className="button-wrap">
+                <DefaultButton
+                    className="card-link"
+                    title="View product"
+                    to={`/product_detail/${this.props.product._id}`}
+                    addStyles={{margin: '10px 0 0 0'}}
+                />
+            </div>
+
             
-            
-        
+            </div>
         </div>
       </div>
     )
