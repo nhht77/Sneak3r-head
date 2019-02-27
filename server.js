9d438bed383e4fe3c9c2b320347734821014cb77
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-02-27 21:30:53 +0200

    Prepare to Deploy

--- server.js
+++ server.js
@@ -30,4 +30,14 @@ app.use('/api/brands', brands);
 app.use('/api/products', products);
 app.use('/api/conditions', conditions);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+}
+
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
